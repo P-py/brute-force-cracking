a7ac41118a98824d1878c2cd92c83d1f827bf800
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-08-06 16:41:38 -0300

    Create brute-force.py

---
+++ brute-force.py
@@ -0,0 +1,31 @@
+import itertools
+import string
+import time
+import random
+
+all = string.ascii_letters + string.digits + string.punctuation
+
+def guess_password(real):
+    start_time = time.perf_counter()
+    chars = string.ascii_letters + string.digits + string.punctuation
+    attempts = 0
+    for password_length in range(1, 9):
+        for guess in itertools.product(chars, repeat=password_length):
+            #time.sleep(.01)	
+            attempts += 1
+            guess = ''.join(guess)
+            if guess == real:
+                final_time = time.perf_counter()
+                time_spent = final_time - start_time
+                return f'HTTP REQUEST ACCEPTED [200]! Password is {guess}. Found in {attempts} guesses and {time_spent:0.4f} seconds.'
+            print(guess, attempts)
+
+#Generates a 4 char length password.
+length = 3
+sample = random.sample(all, length)
+sample = ''.join(sample)
+print(sample)
+time.sleep(10)
+
+#Start trying the passwords.
+print(guess_password(sample))
